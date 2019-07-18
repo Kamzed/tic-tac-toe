18bf909ab0c4d91ba54cd6142587c270367f7a13
Alexandr Chigarev <kamzed1995@gmail.com>
2019-07-18 14:19:43 +0300

    Updates

---
+++ precache-manifest.185356c275e159a0513793e8450d9ceb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "62ff6573020a8a2e947ed91f330f4714",
+    "url": "/tic-tac-toe/index.html"
+  },
+  {
+    "revision": "d949a3f932bdc8971973",
+    "url": "/tic-tac-toe/static/css/main.7dee3a07.chunk.css"
+  },
+  {
+    "revision": "685d0c597e8419129599",
+    "url": "/tic-tac-toe/static/js/2.0e3f6a1a.chunk.js"
+  },
+  {
+    "revision": "d949a3f932bdc8971973",
+    "url": "/tic-tac-toe/static/js/main.23fa651a.chunk.js"
+  },
+  {
+    "revision": "9728fa41a930f12fdc87",
+    "url": "/tic-tac-toe/static/js/runtime~main.f1e29306.js"
+  }
+]);
